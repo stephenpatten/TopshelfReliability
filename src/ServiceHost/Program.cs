4bfff1babc78547db0fae03076402194523309b6
Stephen Patten <stephenpatten@gmail.com>
2017-09-29 07:56:33 -0700

    Throw unhandled exception

--- src/ServiceHost/Program.cs
+++ src/ServiceHost/Program.cs
@@ -13,8 +13,15 @@ namespace ServiceHost
         readonly Timer _timer;
         public TownCrier()
         {
-            _timer = new Timer(1000) { AutoReset = true };
-            _timer.Elapsed += (sender, eventArgs) => Console.WriteLine("It is {0} and all is well", DateTime.Now);
+            _timer = new Timer(1000)
+            {
+                AutoReset = true
+            };
+            _timer.Elapsed += (sender, eventArgs) => {
+                Console.WriteLine("It is {0} and all is well", DateTime.Now);
+                throw new Exception("MyException");
+                }
+            ;
         }
         public void Start() { _timer.Start(); }
         public void Stop() { _timer.Stop(); }
